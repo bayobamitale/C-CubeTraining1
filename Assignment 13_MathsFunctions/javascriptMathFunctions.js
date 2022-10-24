25dcbee570cf5f0b21191234af661ba63e67a499
Tola ADEBAMBO <ttola2006@yahoo.com>
2022-10-24 17:14:56 +0100

    Correction made to the assignment to meet requirement

--- Assignment 13_MathsFunctions/javascriptMathFunctions.js
+++ Assignment 13_MathsFunctions/javascriptMathFunctions.js
@@ -1,14 +1,17 @@
 const arrayFunctions = [(a)=> {return Math.PI * a * a}, 
 						(a) => {return Math.pow(a,2)}, 
 						(a) => {return Math.sqrt(a)},
-						(a) => {return Math.log(a)},
-						() => {return Math.random()}]
+						(a) => {return Math.log10(a)},
+						(a) => {return Math.sin(a * Math.PI/180)}]
 
 for(var x = 1; x <= 3; x++)
 {						
 	for(var i = 0; i < arrayFunctions.length; i++)
 	{
-		console.log(arrayFunctions[i](Math.random() * 100));
+		var parameter = Math.ceil(Math.random() * 10);
+		console.log("Function: " + arrayFunctions[i] + " with argument " + parameter + ", yields: ");
+		console.log(arrayFunctions[i](parameter ));
+		console.log("\t");
 	}
-	console.log("\n");
+	console.log("======================================================================================");
 }						
